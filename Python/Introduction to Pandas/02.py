739b423d9cf0517efb11c7806d83eedd2be19af9
Fahlevi20 <rozakalfi@gmail.com>
2024-01-22 19:50:30 +0700

    accept 02.py

---
+++ Python/Introduction to Pandas/02.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def getDataframeSize(players: pd.DataFrame) -> list[int]:
+    df=pd.DataFrame(players)
+    shape=df.shape
+    print(shape)
+    rows=df.shape[0]
+    cols=df.shape[1]
+    print(f"this dataframe contains {rows} rows and {cols} columns")
+
+    return list(shape)
+
+data = {
+    'player_id': [846, 749, 155, 583, 388, 883, 355, 247, 761, 642],
+    'name': ['Mason', 'Riley', 'Bob', 'Isabella', 'Zachary', 'Ava', 'Violet', 'Thomas', 'Jack', 'Charlie'],
+    'age': [21, 30, 28, 32, 24, 23, 18, 27, 33, 36],
+    'position': ['Forward', 'Winger', 'Striker', 'Goalkeeper', 'Midfielder', 'Defender', 'Striker', 'Striker', 'Midfielder', 'Center-back'],
+    'team': ['RealMadrid', 'Barcelona', 'ManchesterUnited', 'Liverpool', 'BayernMunich', 'Chelsea', 'Juventus', 'ParisSaint-Germain', 'ManchesterCity', 'Arsenal']
+}
+
+getDataframeSize(data)
