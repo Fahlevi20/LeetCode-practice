3c894b0846ec097f89802e7573f8610186e86944
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-05-26 14:53:07 +0700

    Update 01.twosum.py

--- Python/problems/01.twosum.py
+++ Python/problems/01.twosum.py
@@ -1,10 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        terlihat = {} #make dictionary
-        for i in range(len(nums)):
-            pembeda = target-nums[i] #make different from num subtract from target
-            if pembeda in terlihat: # if pembeda value in terlihat value
-                return [terlihat[pembeda],i] #return terlihat value and index i (from nums)
 
-            else:
-                terlihat[nums[i]] = i 
+        prevMap = {}#val:index
+
+        for i,n in enumerate(nums):
+            diff = target-n
+            if diff in prevMap:
+                return [prevMap[diff],i] # [prevmap value, index]
+            prevMap[n]=i
+        return
