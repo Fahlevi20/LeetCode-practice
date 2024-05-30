48ce4e104bdd5d1e704f29e5bfd2534c49a63c8e
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-05-30 09:12:37 +0700

    another version twoSum2.py

--- Python/problems/twoSum2.py
+++ Python/problems/twoSum2.py
@@ -1,14 +1,10 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l = 0
-        r = len(numbers)-1
-
-        while l <r:
-            curSum = numbers[l] + numbers[r]
-            if curSum > target:
-                r -=1
-            elif curSum < target:
-                l +=1
+        l, r = 0, len(numbers)-1
+    
+        while numbers[l]+numbers[r] !=target:
+            if numbers[l]+numbers[r] > target:
+                r-=1
             else:
-                return [l+1, r+1]
-        return []
+                l+=1
+        return [l+1,r+1]
