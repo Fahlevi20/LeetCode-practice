a7c0da0e3f1e074d8af67e9b370e1c115d704521
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-05-31 18:51:38 +0700

    Update maximumSubArray.py

--- Python/problems/maximumSubArray.py
+++ Python/problems/maximumSubArray.py
@@ -1,13 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         msa = nums[0]
-        curS= 0
-        
-        for n in nums:
-            if curS < 0:
-                curS=0
-            curS+=n
-            msa = max(msa,curS)
-        return msa
+        curSum = 0
 
-        
+        for n in nums:
+            if curSum <0:
+                curSum = 0
+            curSum +=n
+            msa = max(msa,curSum)
+        return msa        
