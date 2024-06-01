6a3666d85c524c203581d1e992765a2e0d5503d8
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-01 09:06:49 +0700

    Update validAnagram.py

--- Python/problems/validAnagram.py
+++ Python/problems/validAnagram.py
@@ -2,11 +2,13 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        sumS={}
-        sumT={}
+        
+        countS,countT={},{}
 
         for i in range(len(s)):
-            sumS[s[i]] = 1+sumS.get(s[i],0)
+            countS[s[i]]= 1+countS.get(s[i],0)
         for i in range(len(t)):
-            sumT[t[i]] = 1+ sumT.get(t[i],0)
-        return sumS == sumT
+            countT[t[i]]= 1+countT.get(t[i],0)
+        return countS==countT
+
+            
