b70e1748568283bc4df2db52e544a259cbec84ec
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-04 09:59:57 +0700

    Create test_solution.py

---
+++ Python/problems/Two-Sum/test_solution.py
@@ -0,0 +1,11 @@
+import unittest
+from solution import solution
+class TestSolution(unittest.TestCase):
+  def test_two_sum(self):
+    sol = solution()
+    self.assertEqual (sol.twoSum([2,7,11,15],9),[0,1])
+    self.assertEqual (sol.twoSum([3,2,4],6),[1,2])
+    self.assertEqual (sol.TtwoSum([3,3],6),[0,1])
+
+if __name__== '__main__':
+  unittest.main()
