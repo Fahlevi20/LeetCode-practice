cbc5fd0f4f65f98178b05208ed7263f52b718cc0
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-04 08:25:03 +0700

    Update threeSums.py

--- Python/problems/threeSums.py
+++ Python/problems/threeSums.py
@@ -1,26 +1,24 @@
-nums = [-1,0,1,2,-1,-4]
-res = []
-nums.sort()
-print(nums)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
 
-for i, a in enumerate(nums):
-	
-    if i >0 and a == nums[i-1]:
-     continue
-     
-    l, r = i+1, len(nums)-1
-    print('ini adalah l',l)
-    print('ini r',r)
-    while l<r:
-     threeSum = a+nums[l]+nums[r]
-     if threeSum> 0:
-      r-=1
-      print('after:',r)
-      print(threeSum)
-     elif threeSum <0:
-      l+=1
-      print('after:',l)
-      print(threeSum)
-     else:
-      res.append([a,nums[l],nums[r]])
-     
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
+                continue
+
+            l,r = i+1, len(nums)-1
+
+            while l < r:
+                threeSums = a + nums[l]+nums[r]
+                if threeSums > 0:
+                    r -= 1
+                elif threeSums <0:
+                    l += 1
+                else:
+                    res.append([a,nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l <r:
+                        l += 1 
+        return res
+        
