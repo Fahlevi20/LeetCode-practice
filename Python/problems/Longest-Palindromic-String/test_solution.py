52e71be40cd5d42ab04f1c7dacca5eb77a3565c9
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-05 22:36:26 +0700

    Update test_solution.py

--- Python/problems/Longest-Palindromic-String/test_solution.py
+++ Python/problems/Longest-Palindromic-String/test_solution.py
@@ -3,7 +3,7 @@ from solution import solution
 class TestSolution(unittest.TestCase):
   def longest_palindromic_string(self):
     sol = solution()
-    self.assertEqual (sol.longestPalindrome("babad","bab"),"bab")
+    self.assertEqual (sol.longestPalindrome("---","bab"),"bab")
     self.assertEqual (sol.longestPalindrome("cbbd","bb"),"bb")
 if __name__== '__main__':
   unittest.main()
