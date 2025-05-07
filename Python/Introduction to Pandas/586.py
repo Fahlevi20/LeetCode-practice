caaa34ec471a8b291b87f75105a9fd3a1e471b0e
Fahlevi20 <rozakalfi@gmail.com>
2025-05-07 09:44:56 +0700

    add group by for adding order_count for counting the value in every key

--- Python/Introduction to Pandas/586.py
+++ Python/Introduction to Pandas/586.py
@@ -3,16 +3,19 @@ import pandas as pd
 def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
     df=orders
 
-    counts=df.groupby('customer_number')
-    first_large=df.sort_values(by='customer_number',ascending=False).iloc[[0]]
+    # counts=df.groupby('customer_number')
+    count=df.groupby(by="customer_number").size().reset_index(name='order_count')
+    print(count)
+    first_large=count.sort_values(by='order_count',ascending=False).head(1)
+    return (first_large[['customer_number']])
 
 
-    return first_large[['customer_number']]
+    # return first_large[['customer_number']]
     
 
 data = {
-    'customer_number': [1, 2, 3, 4],
-    'order_number': [1, 2, 3, 3]
+    'customer_number': [1, 2,3,3 ],
+    'order_number': [1, 2, 3, 3.4]
 }
 largest_orders_df = pd.DataFrame(data)
 print(largest_orders(largest_orders_df))
